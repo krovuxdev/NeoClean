6546e3ac84ef9aa8ca793ec8d39a1eb873867982
krovuxdev <62192487+krovuxdev@users.noreply.github.com>
2024-05-31 22:04:29 -0500

    add: telescope keymap Config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -98,6 +98,20 @@ return {
       desc = "Find Plugin File",
     },
     {
+      "<leader>C",
+      function()
+        require("telescope.builtin").find_files({ cwd = vim.fn.stdpath("config") })
+      end,
+      desc = "Find Config",
+    },
+    {
+      "<leader>C/",
+      function()
+        require("telescope.builtin").live_grep({ cwd = vim.fn.stdpath("config") })
+      end,
+      desc = "Find Config",
+    },
+    {
       "<leader><space>",
       function()
         require("telescope.builtin").find_files({ cwd = vim.uv.cwd() })
@@ -189,27 +203,35 @@ return {
     { "<leader>fb", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
 
     -- git
-    { "<leader>fg", "<cmd>Telescope git_files<cr>",                                desc = "Find Files (git-files)" },
-    { "<leader>gc", "<cmd>Telescope git_commits<CR>",                              desc = "Commits" },
-    { "<leader>gs", "<cmd>Telescope git_status<CR>",                               desc = "Status" },
+    {
+      "<leader>fg",
+      "<cmd>Telescope git_files<cr>",
+      desc = "Find Files (git-files)",
+    },
+    { "<leader>gc", "<cmd>Telescope git_commits<CR>",               desc = "Commits" },
+    { "<leader>gs", "<cmd>Telescope git_status<CR>",                desc = "Status" },
     -- search
-    { '<leader>s"', "<cmd>Telescope registers<cr>",                                desc = "Registers" },
-    { "<leader>sa", "<cmd>Telescope autocommands<cr>",                             desc = "Auto Commands" },
-    { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>",                desc = "Buffer" },
-    { "<leader>sc", "<cmd>Telescope command_history<cr>",                          desc = "Command History" },
-    { "<leader>sC", "<cmd>Telescope commands<cr>",                                 desc = "Commands" },
-    { "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<cr>",                      desc = "Document Diagnostics" },
-    { "<leader>sD", "<cmd>Telescope diagnostics<cr>",                              desc = "Workspace Diagnostics" },
-    { "<leader>sh", "<cmd>Telescope help_tags<cr>",                                desc = "Help Pages" },
-    { "<leader>sH", "<cmd>Telescope highlights<cr>",                               desc = "Search Highlight Groups" },
-    { "<leader>sj", "<cmd>Telescope jumplist<cr>",                                 desc = "Jumplist" },
-    { "<leader>sk", "<cmd>Telescope keymaps<cr>",                                  desc = "Key Maps" },
-    { "<leader>sl", "<cmd>Telescope loclist<cr>",                                  desc = "Location List" },
-    { "<leader>sM", "<cmd>Telescope man_pages<cr>",                                desc = "Man Pages" },
-    { "<leader>sm", "<cmd>Telescope marks<cr>",                                    desc = "Jump to Mark" },
-    { "<leader>so", "<cmd>Telescope vim_options<cr>",                              desc = "Options" },
-    { "<leader>sR", "<cmd>Telescope resume<cr>",                                   desc = "Resume" },
-    { "<leader>sq", "<cmd>Telescope quickfix<cr>",                                 desc = "Quickfix List" },
+    { '<leader>s"', "<cmd>Telescope registers<cr>",                 desc = "Registers" },
+    { "<leader>sa", "<cmd>Telescope autocommands<cr>",              desc = "Auto Commands" },
+    { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
+    { "<leader>sc", "<cmd>Telescope command_history<cr>",           desc = "Command History" },
+    { "<leader>sC", "<cmd>Telescope commands<cr>",                  desc = "Commands" },
+    { "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<cr>",       desc = "Document Diagnostics" },
+    { "<leader>sD", "<cmd>Telescope diagnostics<cr>",               desc = "Workspace Diagnostics" },
+    { "<leader>sh", "<cmd>Telescope help_tags<cr>",                 desc = "Help Pages" },
+    {
+      "<leader>sH",
+      "<cmd>Telescope highlights<cr>",
+      desc = "Search Highlight Groups",
+    },
+    { "<leader>sj", "<cmd>Telescope jumplist<cr>",    desc = "Jumplist" },
+    { "<leader>sk", "<cmd>Telescope keymaps<cr>",     desc = "Key Maps" },
+    { "<leader>sl", "<cmd>Telescope loclist<cr>",     desc = "Location List" },
+    { "<leader>sM", "<cmd>Telescope man_pages<cr>",   desc = "Man Pages" },
+    { "<leader>sm", "<cmd>Telescope marks<cr>",       desc = "Jump to Mark" },
+    { "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
+    { "<leader>sR", "<cmd>Telescope resume<cr>",      desc = "Resume" },
+    { "<leader>sq", "<cmd>Telescope quickfix<cr>",    desc = "Quickfix List" },
 
     -- Colorscheme
     {
