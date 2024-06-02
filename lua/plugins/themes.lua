af5c9e21860ff62b34b86ba749fac50655187389
krovuxdev <62192487+krovuxdev@users.noreply.github.com>
2024-06-01 19:14:52 -0500

    feat: Update Themes

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -2,9 +2,11 @@ return {
   -- add gruvbox
   {
     "ellisonleao/gruvbox.nvim",
+    dependencies = {
+      "DeviusVim/deviuspro.nvim",
+    },
     config = function()
       vim.cmd.colorscheme("gruvbox")
     end,
   },
-  { "DeviusVim/deviuspro.nvim" },
 }
