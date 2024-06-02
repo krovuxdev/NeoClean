2b8a89295eacd2ef9c7e9922721288861d38d3e3
krovuxdev <62192487+krovuxdev@users.noreply.github.com>
2024-06-02 14:59:33 -0500

    feat(Rust): Update LSP of rust

--- lua/lang/rust.lua
+++ lua/lang/rust.lua
@@ -1,42 +1,72 @@
 return {
-  "mrcjkb/rustaceanvim",
-  version = "^4",
-  lazy = false,
-  ft = { "rust" },
-  opts = {
-    server = {
-      on_attach = function(_, bufnr)
-        vim.keymap.set("n", "<leader>cR", function()
-          vim.cmd.RustLsp("codeAction")
-        end, { desc = "Code Action", buffer = bufnr })
-        vim.keymap.set("n", "<leader>dr", function()
-          vim.cmd.RustLsp("debuggables")
-        end, { desc = "Rust Debuggables", buffer = bufnr })
+  {
+    -- Rust LSP
+    "mrcjkb/rustaceanvim",
+    version = "^4",
+    lazy = false,
+    ft = { "rust" },
+    dependencies = {
+      "Saecki/crates.nvim",
+      event = { "BufRead Cargo.toml" },
+      config = function()
+        require("cmp").setup({
+          completion = {
+            cmp = { enabled = true },
+          },
+        })
       end,
-      default_settings = {
-        ["rust-analyzer"] = {
-          cargo = {
-            allFeatures = true,
-            loadOutDirsFromCheck = true,
-            buildScripts = {
-              enable = true,
-            },
-          },
-          checkOnSave = {
-            allFeatures = true,
-            command = "clippy",
-            extraArgs = { "--no-deps" },
-          },
-          procMacro = {
-            enable = true,
-            ignored = {
-              ["async-trait"] = { "async_trait" },
-              ["napi-derive"] = { "napi" },
-              ["async-recursion"] = { "async_recursion" },
-            },
-          },
+      opt = {
+        completion = {
+          cmp = { enabled = true },
         },
       },
     },
+    config = function()
+      vim.g.rustaceanvim = {
+        -- Plugin configuration
+        tools = {},
+        -- LSP configuration
+        server = {
+          on_attach = function(client, bufnr)
+            vim.keymap.set("n", "<leader>cR", function()
+              vim.cmd.RustLsp("codeAction")
+            end, { desc = "Code Action", buffer = bufnr })
+            vim.keymap.set("n", "<leader>dr", function()
+              vim.cmd.RustLsp("debuggables")
+            end, { desc = "Rust Debuggables", buffer = bufnr })
+          end,
+
+          default_settings = {
+            ["rust-analyzer"] = {
+              cargo = {
+                allFeatures = true,
+                loadOutDirsFromCheck = true,
+                buildScripts = {
+                  enable = true,
+                },
+              },
+              checkOnSave = {
+                allFeatures = true,
+                command = "clippy",
+                extraArgs = { "--no-deps" },
+              },
+              procMacro = {
+                enable = true,
+                ignored = {
+                  ["async-trait"] = { "async_trait" },
+                  ["napi-derive"] = { "napi" },
+                  ["async-recursion"] = { "async_recursion" },
+                },
+              },
+            },
+          },
+        },
+        dap = {},
+      }
+      local bufnr = vim.api.nvim_get_current_buf()
+      vim.keymap.set("n", "<leader>a", function()
+        vim.cmd.RustLsp("codeAction")
+      end, { silent = true, buffer = bufnr })
+    end,
   },
 }
