97d85c62fee3cd71d0b0a25dccd294ca00517f8d
krovuxdev <62192487+krovuxdev@users.noreply.github.com>
2024-06-08 17:53:59 -0500

    feat: update loaders.lua

--- lua/utils/loaders.lua
+++ lua/utils/loaders.lua
@@ -1,5 +1,9 @@
 local M = {}
-function M.setup(directory)
+function M.setup(config)
+  if type(config) == "string" then
+    config = { import = config }
+  end
+  local directory = config.import
   local dir_path = vim.fn.stdpath("config") .. "/lua/" .. directory
   for _, file in ipairs(vim.fn.globpath(dir_path, "*.lua", false, true)) do
     require(directory .. "." .. file:match("^.+/(.+)%.lua$"))
