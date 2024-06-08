c3c6a750043707a9af730171b346106c97f65127
krovuxdev <62192487+krovuxdev@users.noreply.github.com>
2024-06-07 19:20:06 -0500

    move file 'Lazy.lua' to lua

--- lua/Lazy.lua
+++ lua/Lazy.lua
@@ -10,4 +10,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
+require("lazy").setup({
+  { import = "plugins" },
+  { import = "lang" },
+})
