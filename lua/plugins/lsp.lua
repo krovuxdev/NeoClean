d4c228fce068f277e24ac6b9b9877cb9ccccd81e
krovuxdev <62192487+krovuxdev@users.noreply.github.com>
2024-10-29 22:39:36 -0500

    fix(lsp): update servers configurations

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,128 +1,91 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    event = "VeryLazy",
-    cmd = { "LspInfo", "LspInstall", "LspUninstall", "Mason" },
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = {
       {
         "williamboman/mason.nvim",
-        cmd = "Mason",
+        config = true,
         keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
-        build = ":MasonUpdate",
       },
       "williamboman/mason-lspconfig.nvim",
+      {
+        "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+        config = function()
+          require("lsp_lines").setup()
+        end,
+      },
+      -- "github/copilot.vim",
       "j-hui/fidget.nvim",
-      -- "jose-elias-alvarez/typescript.nvim",
-      { "folke/neoconf.nvim", cmd = "Neoconf", config = false, dependencies = { "nvim-lspconfig" } },
-      { "folke/neodev.nvim",  opts = {} },
+      -- "mfussenegger/nvim-dap",
+      { "folke/neodev.nvim", opts = {} },
     },
-    config = function()
-      local lspconfig = require("lspconfig")
 
+    config = function()
       local mason = require("mason")
       local mason_lspconfig = require("mason-lspconfig")
-      local cmp_lsp = require("cmp_nvim_lsp")
-      lspconfig.emmet_language_server.setup({
-        filetypes = {
-          "css",
-          "eruby",
-          "html",
-          "javascript",
-          "javascriptreact",
-          "less",
-          "sass",
-          "scss",
-          "pug",
-          "typescriptreact",
-        },
-        -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
-        -- **Note:** only the options listed in the table are supported.
-        init_options = {
-          ---@type table<string, string>
-          includeLanguages = {},
-          --- @type string[]
-          excludeLanguages = {},
-          --- @type string[]
-          extensionsPath = {},
-          --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
-          preferences = {},
-          --- @type boolean Defaults to `true`
-          showAbbreviationSuggestions = true,
-          --- @type "always" | "never" Defaults to `"always"`
-          showExpandedAbbreviation = "always",
-          --- @type boolean Defaults to `false`
-          showSuggestionsAsSnippets = false,
-          --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
-          syntaxProfiles = {},
-          --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
-          variables = {},
-        },
-      })
-      require("fidget").setup({})
-      local capabilities =
-          vim.tbl_deep_extend("force", {}, vim.lsp.protocol.make_client_capabilities(), cmp_lsp.default_capabilities())
+      local lspconfig = require("lspconfig")
+
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
       mason.setup({
         ui = {
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
+          border = "rounded",
+          icons = require("config").icons.packages,
+        },
+      })
+      -- require("mason-lspconfig").setup({
+      --   ensure_installed = vim.tbl_keys(servers),
+      -- })
+      mason_lspconfig.setup_handlers({
+        function(server_name)
+          local server_config = require("config.lsp.servers")[server_name] or {}
+          if server_name == "rust_analyzer" then
+            return
+          end
+
+          lspconfig[server_name].setup({
+            capabilities = capabilities,
+            settings = server_config,
+            filetypes = (server_config or {}).filetypes,
+          })
+        end,
+      })
+      lspconfig.nixd.setup({
+        settings = {
+          cmd = { "nixd" },
+          nixd = {
+            nixpkgs = {
+              expr = "import <nixpkgs> { }",
+            },
+            options = {
+              nixos = {
+                expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.k-on.options',
+              },
+              home_manager = {
+                expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."ruixi@k-on".options',
+              },
+            },
           },
         },
       })
+
       require("utils.installed").is_Mason_registry({
+        "emmet-language-server",
+        "bash-language-server",
+        "lua-language-server",
+        "shellcheck",
+        "codelldb",
         "stylua",
         "shfmt",
-        "shellcheck",
-        "emmet-language-server",
-        "stylua",
-        "codelldb",
       })
+
       mason_lspconfig.setup({
         ensure_installed = {
           "rust_analyzer",
-          "lua_ls",
-          "jsonls",
-          "tsserver",
-          "bashls",
           "pyright",
-        },
-        handlers = {
-          function(server_name) -- default handler (optional)
-            require("lspconfig")[server_name].setup({
-              capabilities = capabilities,
-            })
-          end,
-
-          ["html"] = function()
-            lspconfig.html.setup({
-              capabilities = capabilities,
-            })
-          end,
-          ["lua_ls"] = function()
-            lspconfig.lua_ls.setup({
-              capabilities = capabilities,
-              settings = {
-                Lua = {
-                  runtime = { version = "Lua 5.1" },
-                  diagnostics = {
-                    globals = { "vim", "it", "describe", "before_each", "after_each" },
-                  },
-                },
-              },
-            })
-          end,
-        },
-      })
-
-      vim.diagnostic.config({
-        float = {
-          focusable = false,
-          style = "minimal",
-          border = "rounded",
-          source = "always",
-          header = "",
-          prefix = "",
+          "taplo",
         },
       })
     end,
