2b463f67ca447425e0c288356471a3e75114ed5c
krovuxdev <62192487+krovuxdev@users.noreply.github.com>
2024-11-02 19:31:33 -0500

    fix(Presence): update configurations

--- lua/plugins/presence.lua
+++ lua/plugins/presence.lua
@@ -1,39 +1,59 @@
 return {
-  "andweeb/presence.nvim",
-  config = function()
-    return {
-      workspace_text = function(git_project_name, buffer)
-        local project_name = git_project_name
-        if not git_project_name then
-          project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
-        end
-        return string.format("Workspace: %s", project_name)
-      end,
-
-      -- General options
-      auto_update = true, -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
-      neovim_image_text = "Neovim 😘", -- Text displayed when hovered over the Neovim image
-      main_image = "file", -- Main image display (either "neovim" or "file")
-      client_id = "793271441293967371", -- Use your own Discord application client id (not recommended)
-      log_level = nil, -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
-      debounce_timeout = 10, -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
-      enable_line_number = false, -- Displays the current line number instead of the current project
-      blacklist = {}, -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
-      buttons = true,
-      label = "Click",
-      url = "https://github.com/krovuxdev",
-      -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
-      file_assets = {}, -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
-      show_time = true, -- Show the timer
-
-      -- Rich Presence text options
-      editing_text = "Programando %s",       -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
-      file_explorer_text = "Browsing %s",    -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
-      git_commit_text = "Committing changes", -- Format string rendered when committing changes in git (either string or function(filename: string): string)
-      plugin_manager_text = "Managing plugins", -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
-      reading_text = "Reading %s",           -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
-      --workspace_text      = "Working %s",            -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
-      line_number_text = "Line %s out of %s", -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
-    }
-  end,
+  {
+    "vyfor/cord.nvim",
+    event = "VeryLazy",
+    build = "./build",
+    config = function()
+      require("cord").setup({
+        usercmds = true,
+        log_level = nil,      -- One of 'trace', 'debug', 'info', 'warn', 'error', 'off'
+        timer = {
+          interval = 1500,    -- Interval between presence updates in milliseconds (min 500)
+          reset_on_idle = false, -- Reset start timestamp on idle
+          reset_on_change = false, -- Reset start timestamp on presence change
+        },
+        editor = {
+          image = "https://i.pinimg.com/originals/e0/19/dd/e019dd90566b6e72abc8166614eee7e5.gif",
+          client = "neovim", -- vim, neovim, lunarvim, nvchad, astronvim or your application's client id
+          tooltip = "Viva Rust", -- Text to display when hovering over the editor's image
+        },
+        display = {
+          show_time = true,
+          show_repository = true,
+          show_cursor_position = true,
+          swap_fields = false,
+          swap_icons = true,
+          workspace_blacklist = {},
+        },
+        lsp = {
+          show_problem_count = false, -- Display number of diagnostics problems
+          severity = 1,          -- 1 = Error, 2 = Warning, 3 = Info, 4 = Hint
+          scope = "workspace",   -- buffer or workspace
+        },
+        idle = {
+          enable = true,
+          show_status = true,
+          timeout = 0,
+          disable_on_focus = true,
+          text = "Descanso",
+          tooltip = "💤",
+        },
+        text = {
+          viewing = "Viewing {}",
+          editing = "Editing {}",
+          file_browser = "Browsing files in {}",
+          plugin_manager = "Managing plugins in {}",
+          lsp_manager = "Configuring LSP in {}",
+          vcs = "Committing changes in {}",
+          workspace = "",
+        },
+        buttons = {
+          {
+            label = "Push Me",
+            url = "https://github.com/krovuxdev",
+          },
+        },
+      })
+    end,
+  },
 }
