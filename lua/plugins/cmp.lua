5d26c51e1a51e166f9245bd2e357a97e29b3dd85
krovuxdev <62192487+krovuxdev@users.noreply.github.com>
2024-11-05 22:10:56 -0500

    refactor(cmp): changes and improvements

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,114 +1,154 @@
-function slice(tbl, start, finish, step)
-  local sliced = {}
-  for i = start, finish, step or 1 do
-    table.insert(sliced, tbl[i])
-  end
-  return sliced
-end
-
-_G.filter_enabled = false
-_G.name_cmp = {
-  { name = "nvim_lsp" },
-  { name = "buffer" },
-  { name = "copilot" },
-  { name = "path" },
-  { name = "luasnip" },
-}
-function ToggleFilter()
-  local cmp = require("cmp")
-  vim.notify = require("notify")
-  if _G.filter_enabled then
-    cmp.setup({
-      sources = cmp.config.sources({
-        unpack(_G.name_cmp),
-      }),
-    })
-    print("Filtro activado [WORD-CMP]")
-  else
-    cmp.setup({
-      sources = cmp.config.sources({
-        {
-          name = "nvim_lsp",
-          entry_filter = function(entry, ctx)
-            return require("cmp").lsp.CompletionItemKind.Text ~= entry:get_kind()
-          end,
-        },
-        unpack(slice(_G.name_cmp, 2, 5)),
-      }),
-    })
-  end
-  _G.filter_enabled = not _G.filter_enabled
-end
-
 return {
-  {
-    "hrsh7th/nvim-cmp",
-    event = { "BufReadPost", "BufNewFile" },
-    dependencies = {
-      -- CMP
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-cmdline",
-      "saadparwaiz1/cmp_luasnip",
-      --
-      "onsails/lspkind.nvim",
-      "windwp/nvim-ts-autotag",
-      "windwp/nvim-autopairs",
-      "tpope/vim-surround",
-      "Luasnip",
-    },
-    config = function()
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-      local lspkind = require("lspkind")
-      require("nvim-autopairs").setup()
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
+  "hrsh7th/nvim-cmp",
+  event = { "BufReadPost", "BufNewFile" },
+  dependencies = {
+    -- CMP
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-nvim-lsp",
+    "saadparwaiz1/cmp_luasnip",
+    --
+    "onsails/lspkind.nvim",
+    -- "windwp/nvim-ts-autotag",
+    "windwp/nvim-autopairs",
+    "hrsh7th/cmp-vsnip",
+    "hrsh7th/vim-vsnip",
+    "Luasnip",
+  },
+  config = function()
+    local cmp = require("cmp")
+    vim.api.nvim_set_hl(0, "Pmenu", { link = "CmpPmenu" })
+    local pairs_ok, pairs = pcall(require, "nvim-autopairs")
+    if pairs_ok then
+      pairs.setup({
+        check_ts = true,
+        ts_config = {
+          lua = { "string" },
+          javascript = { "template_string" },
+          java = false,
         },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<Tab>"] = cmp.mapping.select_next_item(),
-          ["<s-tab>"] = cmp.mapping.select_prev_item(),
-          ["<c-space>"] = cmp.mapping.complete({}),
-          ["<c-c>"] = cmp.mapping.abort(),
-          ["<cr>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        -- sources = cmp.config.sources({
-        --   unpack(_G.name_cmp),
-        -- }),
-        sources = ToggleFilter(),
-        formatting = {
-          expandable_indicator = true,
-          format = lspkind.cmp_format({
-            mode = "symbol_text",
-            maxwidth = 50,
-            ellipsis_char = "...",
-            symbol_map = {
-              Copilot = "",
-            },
-          }),
-        },
-        experimental = {
-          ghost_text = true,
+        disable_filetype = { "TelescopePrompt", "spectre_panel" },
+        fast_wrap = {
+          map = "<M-e>",
+          chars = { "{", "[", "(", '"', "'" },
+          pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+          offset = 0, -- Offset from pattern match
+          end_key = "$",
+          keys = "qwertyuiopzxcvbnmasdfghjkl",
+          check_comma = true,
+          highlight = "PmenuSel",
+          highlight_grey = "LineNr",
         },
       })
-    end,
-    keys = {
-      {
-        "<leader>t",
-        function()
-          print("Filtro desactivado [WORD-CMP]")
-          ToggleFilter()
-        end,
-        desc = "Toggle Text Filter",
+    end
+    local function border(hl_name)
+      return {
+        { "╭", hl_name },
+        { "─", hl_name },
+        { "╮", hl_name },
+        { "│", hl_name },
+        { "╯", hl_name },
+        { "─", hl_name },
+        { "╰", hl_name },
+        { "│", hl_name },
+      }
+    end
+    -- vim.g.completion_disable_snippets = 1
+    cmp.setup({
+
+      matching = {
+        disallow_fuzzy_matching = true,
+        disallow_fullfuzzy_matching = true,
+        disallow_prefix_unmatching = true,
+        disallow_partial_matching = false,
+        disallow_partial_fuzzy_matching = false,
+        disallow_symbol_nonprefix_matching = false,
       },
-    },
-  },
+
+      snippet = {
+        expand = function(args)
+          vim.snippet.expand(args.body)
+        end,
+      },
+
+      completion = {
+        completeopt = "Pmenu,menuone,noinsert",
+      },
+
+      window = {
+        completion = {
+          side_padding = ("default" ~= "gruvbox-material" and "default" ~= "gruvbox-material") and 1 or 0,
+          keyword_length = 1,
+          winhighlight = "Normal:CmpPmenu,FloatBorder:CmpPmenu,Search:None",
+
+          border = border("CmpDocBorder"),
+          scrollbar = false,
+        },
+
+        documentation = {
+          border = border("CmpDocBorder"),
+          winhighlight = "Normal:CmpDoc",
+        },
+      },
+
+      mapping = cmp.mapping.preset.insert({
+        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<s-tab>"] = cmp.mapping.select_prev_item(),
+        ["<c-space>"] = cmp.mapping.complete(),
+        ["<c-c>"] = cmp.mapping.abort(),
+        ["<cr>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+      }),
+
+      sources = cmp.config.sources({
+
+        { name = "vsnip" },
+        { name = "path" },
+        -- { name = "copilot" },
+      }, {
+        {
+          name = "nvim_lsp",
+
+          keyword_length = 3,
+          entry_filter = function(entry, _)
+            local kind = entry:get_kind()
+            local allowed_filetypes = { "html", "css", "scss" }
+            local filetype = vim.bo.filetype
+
+            if vim.tbl_contains(allowed_filetypes, filetype) then
+              return true
+            end
+            if kind == 15 or kind == 1 then
+              return false
+            end
+            return true
+          end,
+        },
+        {
+          name = "buffer",
+        },
+
+        { name = "luasnip" },
+      }),
+
+      formatting = {
+        expandable_indicator = true,
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+          local kind = vim_item.kind
+          local icons_kind = require("config")
+          vim_item.kind = " " .. (icons_kind.icons.kinds[kind] or "?")
+          vim_item.menu = "   " .. entry.source.name
+          -- vim_item.abbr = vim_item.abbr:match("[^(]+")
+          local source = entry.source.name
+          if source == "nvim_lsp" then
+            vim_item.dup = 1
+          end
+          return vim_item
+        end,
+      },
+      experimental = {
+        ghost_text = true,
+      },
+    })
+  end,
 }
