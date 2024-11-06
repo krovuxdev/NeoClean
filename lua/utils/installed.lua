1fdf17565f7684bd93473053e22d03aa7445e407
krovuxdev <62192487+krovuxdev@users.noreply.github.com>
2024-11-05 22:19:24 -0500

    fix(utils): correct package installation condition

--- lua/utils/installed.lua
+++ lua/utils/installed.lua
@@ -3,8 +3,10 @@ function M.is_Mason_registry(pack)
   local mason_registry = require("mason-registry")
   for _, p in ipairs(pack) do
     local status, package = pcall(mason_registry.get_package, p)
-    if not package:is_installed() then
-      vim.cmd("MasonInstall " .. p)
+    if status and package then
+      if not package:is_installed() then
+        vim.cmd("MasonInstall --force " .. p)
+      end
     end
   end
 end
