31835d67826bd84bcf0c077f03309b119d79812c
krovuxdev <62192487+krovuxdev@users.noreply.github.com>
2024-11-06 00:52:01 -0500

    fix(utils): Loaders imports without lazy

--- lua/utils/loaders.lua
+++ lua/utils/loaders.lua
@@ -6,8 +6,9 @@ function M.setup(config)
   local directory = config.import
   local dir_path = vim.fn.stdpath("config") .. "/lua/" .. directory
   for _, file in ipairs(vim.fn.globpath(dir_path, "*.lua", false, true)) do
-    require(directory .. "." .. file:match("^.+/(.+)%.lua$"))
+    require(directory .. "." .. (file:match("^.+/([^/]+)%.lua$") or ""):gsub("^Lazy$", ""))
   end
+  require(directory .. ".Lazy")
 end
 
 return M
