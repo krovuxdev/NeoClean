8c5ac7c1614e0e7f2de78be211856e70f76b264b
krovuxdev <62192487+krovuxdev@users.noreply.github.com>
2024-11-08 23:39:00 -0500

    fix(lsp): update capabilites and append server

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,93 +1,60 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      {
-        "williamboman/mason.nvim",
-        config = true,
-        keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
-      },
-      "williamboman/mason-lspconfig.nvim",
-      {
-        "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-        config = function()
-          require("lsp_lines").setup()
-        end,
-      },
-      -- "github/copilot.vim",
-      "j-hui/fidget.nvim",
-      -- "mfussenegger/nvim-dap",
-      { "folke/neodev.nvim", opts = {} },
-    },
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPost" },
+		dependencies = {
+			{
+				"williamboman/mason.nvim",
+				keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+				opts = {
+					ui = {
+						border = "rounded",
+						icons = require("config").icons.packages,
+					},
+				},
+			},
+			"williamboman/mason-lspconfig.nvim",
+			{
+				"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+				config = function()
+					require("lsp_lines").setup()
+				end,
+			},
+		},
+		config = function()
+			-- local mason = require("mason")
+			local mason_lspconfig = require("mason-lspconfig")
+			local lspconfig = require("lspconfig")
+			for server_name, cfg in pairs(require("config.lsp.servers").lsp) do
+				local server_config = cfg or {}
+				server_config.capabilities =
+					vim.lsp.protocol.make_client_capabilities()
+				server_config.on_attach =
+					require("config.lsp.on_attach").on_attach
+				lspconfig[server_name].setup(server_config)
+			end
+			-- Lo pensaré eso :D
+			-- require("mason-lspconfig").setup({
+			--   ensure_installed = vim.tbl_keys(servers),
+			-- })
+			require("utils.installed").is_Mason_registry({
+				"emmet-language-server",
+				"bash-language-server",
+				"lua-language-server",
+				"shellcheck",
+				"codelldb",
+				"stylua",
+				"shfmt",
+			})
 
-    config = function()
-      local mason = require("mason")
-      local mason_lspconfig = require("mason-lspconfig")
-      local lspconfig = require("lspconfig")
-
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-
-      mason.setup({
-        ui = {
-          border = "rounded",
-          icons = require("config").icons.packages,
-        },
-      })
-      -- require("mason-lspconfig").setup({
-      --   ensure_installed = vim.tbl_keys(servers),
-      -- })
-      mason_lspconfig.setup_handlers({
-        function(server_name)
-          local server_config = require("config.lsp.servers")[server_name] or {}
-          if server_name == "rust_analyzer" then
-            return
-          end
-
-          lspconfig[server_name].setup({
-            capabilities = capabilities,
-            settings = server_config,
-            filetypes = (server_config or {}).filetypes,
-          })
-        end,
-      })
-      lspconfig.nixd.setup({
-        settings = {
-          cmd = { "nixd" },
-          nixd = {
-            nixpkgs = {
-              expr = "import <nixpkgs> { }",
-            },
-            options = {
-              nixos = {
-                expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.k-on.options',
-              },
-              home_manager = {
-                expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."ruixi@k-on".options',
-              },
-            },
-          },
-        },
-      })
-
-      require("utils.installed").is_Mason_registry({
-        "emmet-language-server",
-        "bash-language-server",
-        "lua-language-server",
-        "shellcheck",
-        "codelldb",
-        "stylua",
-        "shfmt",
-      })
-
-      mason_lspconfig.setup({
-        ensure_installed = {
-          "rust_analyzer",
-          "pyright",
-          "taplo",
-        },
-      })
-    end,
-  },
+			mason_lspconfig.setup({
+				ensure_installed = {
+					"rust_analyzer",
+					"html",
+					"pyright",
+					"taplo",
+				},
+			})
+		end,
+	},
 }
