4817ef747aeb1382a292d0925198e811093477c6
krovuxdev <62192487+krovuxdev@users.noreply.github.com>
2024-11-08 23:57:29 -0500

    feat(config): add options for custom neovim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -39,48 +39,48 @@ vim.g.autoformat = true
 vim.opt.wrap = false
 
 vim.diagnostic.config({
-  signs = {
-    text = {
-      [vim.diagnostic.severity.ERROR] = diagnostic_icons.Error,
-      [vim.diagnostic.severity.WARN] = diagnostic_icons.Warn,
-      [vim.diagnostic.severity.HINT] = diagnostic_icons.Hint,
-      [vim.diagnostic.severity.INFO] = diagnostic_icons.Info,
-    },
-  },
-  update_in_insert = true,
-  virtual_text = false,
-  virtual_lines = true,
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = diagnostic_icons.Error,
+			[vim.diagnostic.severity.WARN] = diagnostic_icons.Warn,
+			[vim.diagnostic.severity.HINT] = diagnostic_icons.Hint,
+			[vim.diagnostic.severity.INFO] = diagnostic_icons.Info,
+		},
+	},
+	update_in_insert = true,
+	virtual_text = false,
+	virtual_lines = true,
 
-  float = {
-    focusable = false,
-    max_width = 80,
-    wrap = true,
-    style = "minimal",
-    border = "rounded",
-    source = "if_many",
-    header = "",
-    scope = "cursor",
-    prefix = "●",
-  },
+	float = {
+		focusable = false,
+		max_width = 80,
+		wrap = true,
+		style = "minimal",
+		border = "rounded",
+		source = "if_many",
+		header = "",
+		scope = "cursor",
+		-- prefix = "●",
+	},
 })
 
 --
 require("config.statuscolumn").setup({
-  mode = "hybrid",
-  iconRelnum = " ",
-  base16 = {
-    base00 = "#fbf1c7",
-    base01 = "#e9d5a4",
-    base02 = "#d7b980",
-    base03 = "#c5a25d",
-    base04 = "#b4923a",
-    base05 = "#a48024",
-    base06 = "#8c6b1b",
-    base07 = "#73510f",
-    base08 = "#5a3a08",
-    base09 = "#593a0a",
+	mode = "hybrid",
+	iconRelnum = " ",
+	base16 = {
+		base00 = "#fbf1c7",
+		base01 = "#e9d5a4",
+		base02 = "#d7b980",
+		base03 = "#c5a25d",
+		base04 = "#b4923a",
+		base05 = "#a48024",
+		base06 = "#8c6b1b",
+		base07 = "#73510f",
+		base08 = "#5a3a08",
+		base09 = "#593a0a",
 
-    -- "#3b3b3b",
-  },
-  -- textr = "│",
+		-- "#3b3b3b",
+	},
+	-- textr = "│",
 })
