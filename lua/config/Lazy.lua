1ddba0347806e3f9dfc7320f07790b8b6a72d3ed
krovuxdev <62192487+krovuxdev@users.noreply.github.com>
2024-11-09 20:31:59 -0500

    chore: change configuration Lazy

--- lua/config/Lazy.lua
+++ lua/config/Lazy.lua
@@ -1,18 +1,28 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 require("lazy").setup({
-  { import = "plugins" },
-  {
-    import = "ftplugins",
-  },
+	spec = {
+		import = "plugins",
+	},
+	change_detection = { notify = false },
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	ui = {
+		border = "rounded",
+	},
 })
