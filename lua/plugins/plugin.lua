4c9d78f65c51d58c7899f0b64b8559618712f1e2
krovuxdev <62192487+krovuxdev@users.noreply.github.com>
2024-11-10 00:46:15 -0500

    LOVE NEOVIM ❤️

--- lua/plugins/plugin.lua
+++ lua/plugins/plugin.lua
@@ -1,11 +1,1 @@
-return {
-  {
-    "echasnovski/mini.pairs",
-    event = "VeryLazy",
-    opts = {
-      mappings = {
-        ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\`].", register = { cr = false } },
-      },
-    },
-  },
-}
+return {}
