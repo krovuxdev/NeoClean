e9c237f9346b4c51366a0c6141a459f3f61ffc98
krovuxdev <62192487+krovuxdev@users.noreply.github.com>
2024-11-09 20:43:33 -0500

    chore: add keymap more

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,27 +1,31 @@
-local keymap = vim.keymap
+local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
-keymap.set("i", "<S-Tab>", "<C-d>", opts)
-keymap.set("n", "<C-s>", ":w<CR>", opts)
-keymap.set("n", "<C-S-s>", ":wa<CR>", opts)
-keymap.set("n", "<M-.>", ":CommentToggle<CR>", opts)
-keymap.set("v", "<M-.>", ":CommentToggle<CR>", opts)
---
-keymap.set("n", "+", "<C-a>", opts)
-keymap.set("n", "-", "<C-x>", opts)
---
-keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv", opts)
-keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv", opts)
---
-keymap.set("n", "<C-a>", "gg<S-v>G")
-keymap.set("v", "<C-a>", "<Esc>")
---
-keymap.set("n", "ss", ":split<Return>", opts)
-keymap.set("n", "sv", ":vsplit<Return>", opts)
---
-keymap.set("n", "<esc>", ":noh<CR>", opts)
-keymap.set("n", "<C-x>", ":bd<CR>", opts)
---
-keymap.set("n", "<M-h>", ":bprevious<CR>", opts)
-keymap.set("n", "<M-l>", ":bnext<CR>", opts)
---
+map("i", "<S-Tab>", "<C-d>", opts)
+map("n", "<C-s>", ":w<CR>", opts)
+map("n", "<C-S-s>", ":wa<CR>", opts)
+map("n", "<M-.>", ":CommentToggle<CR>", opts)
+map("v", "<M-.>", ":CommentToggle<CR>", opts)
+
+map("n", "+", "<C-a>", opts)
+map("n", "-", "<C-x>", opts)
+
+map("v", "<C-j>", ":m '>+1<CR>gv=gv", opts)
+map("v", "<C-k>", ":m '<-2<CR>gv=gv", opts)
+
+map("n", "<C-a>", "gg<S-v>G")
+map("v", "<C-a>", "<Esc>")
+
+map("n", "ss", ":split<Return>", opts)
+map("n", "sv", ":vsplit<Return>", opts)
+
+map("n", "<esc>", ":noh<CR>", opts)
+map("n", "<C-x>", ":bd<CR>", opts)
+
+map("n", "<M-h>", ":bprevious<CR>", opts)
+map("n", "<M-l>", ":bnext<CR>", opts)
+map("n", "<c-t>", ":tabnew<CR>", opts)
+map("n", "tt", ":tabnew<CR>", opts)
+map("n", "tr", ":tabclose<CR>", opts)
+map("n", "tj", ":tabprevious<CR>", opts)
+map("n", "tk", ":tabnext<CR>", opts)
