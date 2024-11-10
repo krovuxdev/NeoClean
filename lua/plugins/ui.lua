eb13eb45324c132b75c17eced6fc364e234e6253
krovuxdev <62192487+krovuxdev@users.noreply.github.com>
2024-11-09 19:44:39 -0500

    fix(ui): Update UI

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,247 +1,238 @@
 return {
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    opts = {
-      plugins = { spelling = true },
-      defaults = {
-        mode = { "n", "v" },
-        ["g"] = { name = "+goto" },
-        ["gs"] = { name = "+surround" },
-        ["z"] = { name = "+fold" },
-        ["]"] = { name = "+next" },
-        ["["] = { name = "+prev" },
-        ["<leader>sn"] = { name = "+noice" },
-        ["<leader><tab>"] = { name = "+tabs" },
-        ["<leader>b"] = { name = "+buffer" },
-        ["<leader>c"] = { name = "+code" },
-        ["<leader>f"] = { name = "+file/find" },
-        ["<leader>g"] = { name = "+git" },
-        ["<leader>gh"] = { name = "+hunks" },
-        ["<leader>q"] = { name = "+quit/session" },
-        ["<leader>s"] = { name = "+search" },
-        ["<leader>u"] = { name = "+ui" },
-        ["<leader>w"] = { name = "+windows" },
-        ["<leader>x"] = { name = "+diagnostics/quickfix" },
-      },
-    },
-    config = function(_, opts)
-      local wk = require("which-key")
-      wk.setup(opts)
-      wk.register(opts.defaults)
-    end,
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    opts = {
-      signs = {
-        add = { text = "▎" },
-        change = { text = "▎" },
-        delete = { text = "" },
-        topdelete = { text = "" },
-        changedelete = { text = "▎" },
-        untracked = { text = "▎" },
-      },
-      on_attach = function(buffer)
-        local gs = package.loaded.gitsigns
+	"Bekaboo/dropbar.nvim",
+	"nvim-tree/nvim-web-devicons",
+	-- "echasnovski/mini.icons",
+	{
+		"folke/which-key.nvim",
+		dependencies = {},
+		config = function()
+			local wk = require("which-key")
+			wk.setup({
+				preset = "helix",
+				plugins = {
+					marks = false,
+					registers = false,
+					spelling = {
+						enabled = true,
+						suggestions = 20,
+					},
+					presets = {
+						operators = false, -- adds help for operators like d, y, ...
+						motions = false, -- adds help for motions
+						text_objects = false, -- help for text objects triggered after entering an operator
+						windows = false, -- default bindings on <c-w>
+						nav = false, -- misc bindings to work with windows
+						z = false, -- bindings for folds, spelling and others prefixed with z
+						g = false, -- bindings for prefixed with g
+					},
+				},
+				win = {
+					title_pos = "center",
+					no_overlap = true,
+					title = true,
+					border = "single", -- none, single, double, shadow
+					padding = { 2, 2 },
+					zindex = 1000,
+					wo = {
+						winblend = 10,
+					},
+				},
+				layout = {
+					height = { min = 4, max = 25 },
+					width = { min = 20, max = 50 },
+					spacing = 3,
+					align = "left",
+				},
+				sort = { "local", "order", "group", "alphanum", "mod" },
+				show_help = true,
+				show_keys = true,
+				disable = {
+					ft = {},
+					bt = { "TelescopePrompt" },
+				},
+			})
+			require("config.which-key")
+		end,
+	},
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		dependencies = {},
+		view = "cmdline_popup",
+		config = function()
+			require("noice").setup({
+				views = {
+					cmdline_popup = {
+						border = {
+							style = "rounded",
+							padding = { 0, 1 },
+						},
+						position = {
+							row = 5,
+							col = "50%",
+						},
+						size = {
+							width = 60,
+							height = "auto",
+						},
+					},
+					popupmenu = {
+						relative = "editor",
+						position = {
+							row = 8,
+							col = "50%",
+						},
+						size = {
+							width = 60,
+							height = 10,
+						},
+						border = {
+							style = "rounded",
+							padding = { 0, 1 },
+						},
+						win_options = {
+							winhighlight = {
+								Normal = "Normal",
+								FloatBorder = "DiagnosticInfo",
+							},
+						},
+					},
+				},
+			})
+		end,
+	},
 
-        local function map(mode, l, r, desc)
-          vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
-        end
-        -- stylua: ignore start
-        map("n", "]h", function() gs.nav_hunk("next") end, "Next Hunk")
-        map("n", "[h", function() gs.nav_hunk("prev") end, "Prev Hunk")
-        map("n", "]H", function() gs.nav_hunk("last") end, "Last Hunk")
-        map("n", "[H", function() gs.nav_hunk("first") end, "First Hunk")
-        map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
-        map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
-        map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
-        map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
-        map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
-        map("n", "<leader>ghp", gs.preview_hunk_inline, "Preview Hunk Inline")
-        map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
-        map("n", "<leader>ghd", gs.diffthis, "Diff This")
-        map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
-        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
-      end,
-    },
-  },
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-      lsp = {
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-      },
-      routes = {
-        {
-          filter = {
-            event = "msg_show",
-            any = {
-              { find = "%d+L, %d+B" },
-              { find = "; after #%d+" },
-              { find = "; before #%d+" },
-            },
-          },
-          view = "mini",
-        },
-      },
-      presets = {
-        bottom_search = true,
-        command_palette = true,
-        long_message_to_split = true,
-        inc_rename = true,
-      },
-    },
-    -- stylua: ignore
-    keys = {
-      { "<S-Enter>",   function() require("noice").redirect(vim.fn.getcmdline()) end,                 mode = "c",                 desc = "Redirect Cmdline" },
-      { "<leader>snl", function() require("noice").cmd("last") end,                                   desc = "Noice Last Message" },
-      { "<leader>snh", function() require("noice").cmd("history") end,                                desc = "Noice History" },
-      { "<leader>sna", function() require("noice").cmd("all") end,                                    desc = "Noice All" },
-      { "<leader>snd", function() require("noice").cmd("dismiss") end,                                desc = "Dismiss All" },
-      { "<leader>snt", function() require("noice").cmd("telescope") end,                              desc = "Noice Telescope" },
-      { "<c-f>",       function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end,  silent = true,              expr = true,              desc = "Scroll Forward",  mode = { "i", "n", "s" } },
-      { "<c-b>",       function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true,              expr = true,              desc = "Scroll Backward", mode = { "i", "n", "s" } },
-    },
-  },
-  -- icons
-  { "nvim-tree/nvim-web-devicons", lazy = true },
+	{
+		"goolord/alpha-nvim",
+		event = "VimEnter",
+		config = function()
+			local alpha = require("alpha")
+			local dashboard = require("alpha.themes.dashboard")
+			local colors = require("config").colors
+			dashboard.section.header.val =
+				vim.split(require("config").logos[1], "\n")
+			local function pad(n)
+				return { type = "padding", val = n }
+			end
+			local function title(name)
+				return {
+					type = "text",
+					val = name .. "                                          ",
+					opts = { position = "center", hl = colors.purple },
+				}
+			end
+			local function sectionT(name, padding)
+				return {
+					type = "group",
+					val = { pad(padding), title(name) },
+					opts = { position = "center" },
+				}
+			end
+			local Sections = {
+				pad(10),
+				dashboard.section.header,
+				sectionT("Telescope", 1),
+				{
+					type = "group",
+					val = {
+						dashboard.button(
+							"f",
+							"󰈞 " .. " Find file",
+							":Telescope find_files no_ignore=true <CR>"
+						),
+						dashboard.button(
+							"r",
+							" " .. " Recent files",
+							":Telescope oldfiles <CR>"
+						),
+						dashboard.button(
+							"g",
+							" " .. " Grep Text",
+							":Telescope live_grep <CR>"
+						),
+					},
+					opts = { position = "center" },
+				},
 
-  -- ui components
-  { "MunifTanjim/nui.nvim",        lazy = true },
-  {
-    "rcarriga/nvim-notify",
-    keys = {
-      {
-        "<leader>un",
-        function()
-          require("notify").dismiss({ silent = true, pending = true })
-        end,
-        desc = "Dismiss All Notifications",
-      },
-    },
-    opts = {
-      stages = "static",
-      timeout = 3000,
-      max_height = function()
-        return math.floor(vim.o.lines * 0.75)
-      end,
-      max_width = function()
-        return math.floor(vim.o.columns * 0.75)
-      end,
-      on_open = function(win)
-        vim.api.nvim_win_set_config(win, { zindex = 100 })
-      end,
-    },
-  },
-  { "nvimdev/dashboard-nvim", enabled = false },
-  { "goolord/alpha-nvim",     enabled = false },
-  {
-    "echasnovski/mini.starter",
-    version = false,
-    -- lazy = false, -- As https://github.com/nvimdev/dashboard-nvim/pull/450, dashboard-nvim shouldn't be lazy-loaded to properly handle stdin.
-    event = "VimEnter",
-    opts = function()
-      local logo = table.concat({
-        "                 ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆        ",
-        "                ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ",
-        "                      ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ",
-        "                       ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ",
-        "                      ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ",
-        "               ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ",
-        "              ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ",
-        "             ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ",
-        "             ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ",
-        "                  ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ",
-        "                   ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ",
-      }, "\n")
-      local pad = string.rep(" ", 22)
-      local new_section = function(name, action, section)
-        return { name = name, action = action, section = pad .. section }
-      end
+				sectionT("Config   ", 1),
+				{
+					type = "group",
+					val = {
+						dashboard.button(
+							"c",
+							" " .. " Config",
+							":lua require('telescope.builtin').find_files({cwd = vim.fn.stdpath('config')}) <CR>"
+						),
+						dashboard.button("l", "󱓓 " .. " Lazy", ":Lazy<CR>"),
+					},
+					opts = { position = "center" },
+				},
+				sectionT("Built-in", 1),
+				{
+					type = "group",
+					val = {
+						dashboard.button(
+							"n",
+							" " .. " New file",
+							":ene | startinsert<CR>"
+						),
+						dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+					},
+					opts = { position = "center" },
+				},
+				pad(2),
+				dashboard.section.footer,
+			}
 
-      local starter = require("mini.starter")
-      --stylua: ignore
-      local config = {
-        evaluate_single = true,
-        header = logo,
-        items = {
-          new_section("Find file", "Telescope find_files", "Telescope"),
-          new_section("Recent files", "Telescope oldfiles", "Telescope"),
-          new_section("Grep text", "Telescope live_grep", "Telescope"),
-          new_section("Config", "lua require('telescope.builtin').find_files({cwd = vim.fn.stdpath('config')})", "Config"),
-          new_section("Extras", "LazyExtras", "Config"),
-          new_section("Lazy", "Lazy", "Config"),
-          new_section("New file", "ene | startinsert", "Built-in"),
-          new_section("Quit", "qa", "Built-in"),
-          new_section("Session restore", [[lua require("persistence").load()]], "Session"),
-        },
-        content_hooks = {
-          starter.gen_hook.adding_bullet(pad .. "░ ", false),
-          starter.gen_hook.aligning("center", "center"),
-        },
-      }
-      return config
-    end,
-    config = function(_, config)
-      -- close Lazy and re-open when starter is ready
-      if vim.o.filetype == "lazy" then
-        vim.cmd.close()
-        vim.api.nvim_create_autocmd("User", {
-          pattern = "MiniStarterOpened",
-          callback = function()
-            require("lazy").show()
-          end,
-        })
-      end
+			alpha.setup({
+				layout = Sections,
+			})
 
-      local starter = require("mini.starter")
-      starter.setup(config)
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "LazyVimStarted",
-        callback = function(ev)
-          local stats = require("lazy").stats()
-          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-          local pad_footer = string.rep(" ", 8)
-          starter.config.footer = pad_footer
-              .. "⚡ Neovim loaded "
-              .. stats.count
-              .. " plugins in "
-              .. ms
-              .. "ms"
-          if vim.bo[ev.buf].filetype == "starter" then
-            pcall(starter.refresh)
-          end
-        end,
-      })
-    end,
-  },
-  {
-    "folke/persistence.nvim",
-    event = "BufReadPre",
-    opts = { options = vim.opt.sessionoptions:get() },
-    -- stylua: ignore
-    keys = {
-      { "<leader>qs", function() require("persistence").load() end,                desc = "Restore Session" },
-      { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
-      { "<leader>qd", function() require("persistence").stop() end,                desc = "Don't Save Current Session" },
-    },
-  },
-  { "nvim-lua/plenary.nvim", lazy = true },
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("lualine").setup({
-        options = {
-          theme = vim.g.colors_name,
-        },
-      })
-    end,
-  },
+			vim.api.nvim_create_autocmd("User", {
+				pattern = { "AlphaReady", "LazyVimStarted" },
+				callback = function(event)
+					if vim.o.filetype == "lazy" then
+						require("lazy").show()
+					end
+
+					if vim.o.filetype == "alpha" then
+						vim.opt.guicursor:append("a:Cursor/lCursor")
+						require("utils.select").setup(event.buf, "rounded")
+					end
+					local stats = require("lazy").stats()
+					local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+					dashboard.section.footer.val = "⚡ Neovim loaded "
+						.. stats.count
+						.. " plugins in "
+						.. ms
+						.. "ms"
+					pcall(vim.cmd.alphaRedraw)
+				end,
+			})
+			vim.api.nvim_create_autocmd("CmdlineEnter", {
+				callback = function()
+					vim.opt.guicursor =
+						"n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
+				end,
+			})
+			vim.api.nvim_create_autocmd("CmdlineLeave", {
+				callback = function()
+					if vim.o.filetype == "alpha" then
+						vim.opt.guicursor:append("a:Cursor/lCursor")
+					end
+				end,
+			})
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = vim.g.colors_name,
+				},
+			})
+		end,
+	},
 }
