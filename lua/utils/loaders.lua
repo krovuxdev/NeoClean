354c79a92164b86e60c672ceecd60a82047892d3
krovuxdev <62192487+krovuxdev@users.noreply.github.com>
2024-11-09 19:43:51 -0500

    fix(util): fixed loader for invoke

--- lua/utils/loaders.lua
+++ lua/utils/loaders.lua
@@ -1,14 +1,19 @@
 local M = {}
-function M.setup(config)
-  if type(config) == "string" then
-    config = { import = config }
-  end
-  local directory = config.import
-  local dir_path = vim.fn.stdpath("config") .. "/lua/" .. directory
-  for _, file in ipairs(vim.fn.globpath(dir_path, "*.lua", false, true)) do
-    require(directory .. "." .. (file:match("^.+/([^/]+)%.lua$") or ""):gsub("^Lazy$", ""))
-  end
-  require(directory .. ".Lazy")
+function M.setup(config, manager)
+	if type(config) == "string" then
+		config = { import = config }
+	end
+	local directory = config.import
+
+	require(directory .. "." .. manager)
+	local dir_path = vim.fn.stdpath("config") .. "/lua/" .. directory
+	for _, file in ipairs(vim.fn.globpath(dir_path, "*.lua", false, true)) do
+		require(
+			directory
+				.. "."
+				.. (file:match("^.+/([^/]+)%.lua$") or ""):gsub("^Lazy$", "")
+		)
+	end
 end
 
 return M
